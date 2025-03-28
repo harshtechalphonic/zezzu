7fff90f792a0800f84d67a2aa6c4e20c0dcf9404
harsh-techalphonic <harsh.techalphonic@gmail.com>
2025-03-28 18:14:54 +0530

    Complete home page mad

--- src/Components/Home/SuperSaving/SuperSaving.jsx
+++ src/Components/Home/SuperSaving/SuperSaving.jsx
@@ -1,35 +1,14 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import Slider from "react-slick";
 import './SuperSaving.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faArrowRightLong, faBagShopping, faStar, faStarHalfAlt } from '@fortawesome/free-solid-svg-icons';
-import { faHeart as faRegularHeart } from '@fortawesome/free-regular-svg-icons';
-import { faHeart as faSolidHeart } from '@fortawesome/free-solid-svg-icons';
+import { faArrowRightLong } from '@fortawesome/free-solid-svg-icons';
 import { Link } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
-import { wishlistAction } from '../../../store/Categories/wishlistSlice';
+import { useSelector } from 'react-redux';
+import SingleProductSlide from '../../Product/SingleProductSlide';
 
 export default function SuperSaving() {
   const fetch_products = useSelector((store) => store.products);
-  const [products, setProducts] = useState([]);
-  const [wishlist, setWishlist] = useState([]);
-  const dispatch = useDispatch();
-  useEffect(() => {
-      setProducts(fetch_products.data);
-      if(localStorage.getItem("wishlist")){
-        setWishlist([...JSON.parse(localStorage.getItem("wishlist"))]);
-      }
-    }, [fetch_products.status]);
-    
-    const toggleWishlist = (id) => {
-      let wishlist = JSON.parse(localStorage.getItem("wishlist")) || [];
-      wishlist = wishlist.includes(id) ? wishlist.filter(num => num !== id) : [id, ...wishlist];
-      setWishlist(wishlist);
-      localStorage.setItem("wishlist", JSON.stringify(wishlist));
-      dispatch(wishlistAction.addWishlist(wishlist.length));
-    };
-
-
       var settings = {
         dots: false,
         infinite: true,
@@ -80,51 +59,8 @@ export default function SuperSaving() {
 
             <div className='featureslider_one my-4'>
             <Slider {...settings} className="xyzg-slider">
-              {products.filter(e => e.type == 2).map((product) => (
-                            <div key={product.prd_id} className="feature-card">
-                              <span className="disco">
-                                {Math.round(
-                                  ((product.price - product.discount_price) / product.price) *
-                                    100
-                                )}
-                                %
-                              </span>
-                              <span
-                                className="wishicon"
-                                onClick={() => toggleWishlist(product.prd_id)}
-                                style={{ cursor: "pointer", fontSize: "16px" }}
-                              >
-                                <FontAwesomeIcon
-                                  icon={
-                                    wishlist.includes(product.prd_id) ? faSolidHeart : faRegularHeart
-                                  }
-                                  color={wishlist.includes(product.prd_id) ? "red" : "black"}
-                                />
-                              </span>
-                              <div className="card-img">
-                                <img src={product.img_url} alt="Product" />
-                              </div>
-                              <div className="product-detail">
-                                <h3>
-                                  <Link to={`/product/${product.slug}`}>{product.title}</Link>
-                                </h3>
-                                <div className="rating d-flex align-items-center ">
-                                  <FontAwesomeIcon key={0} icon={faStar} />
-                                  <FontAwesomeIcon icon={faStar} />
-                                  <FontAwesomeIcon icon={faStar} />
-                                  <FontAwesomeIcon icon={faStarHalfAlt} />
-                                  <span>({product.avg_ratting})</span>
-                                </div>
-                                <div className="Pricing d-flex align-items-center ">
-                                  <p className="price">₹ {product.discount_price} </p>
-                                  <p className="slashPrice">₹ {product.price} </p>
-                                </div>
-                              </div>
-                              <a href="#!" className="cart-btn">
-                                Add to Cart{" "}
-                                <FontAwesomeIcon icon={faBagShopping} className="ms-2" />
-                              </a>
-                            </div>
+              {fetch_products.data.filter(e => e.type == 2).map((product,index) => (
+                            <SingleProductSlide key={index} product={product}/>
                           ))}
             </Slider>
             </div>
