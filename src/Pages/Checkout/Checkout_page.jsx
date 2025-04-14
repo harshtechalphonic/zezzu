767fea1bb7281895e8a29dbe41958109aec16243
akashdeeptechalphonic <akashdeep@techalphonic.com>
2025-04-14 16:10:46 +0530

    merge harsh code

--- src/Pages/Checkout/Checkout_page.jsx
+++ src/Pages/Checkout/Checkout_page.jsx
@@ -1,13 +1,3 @@
-<<<<<<< HEAD
-import React, { useEffect, useState } from "react";
-import Header from "../../Components/Partials/Header/Header";
-import Footer from "../../Components/Partials/Footer/Footer";
-import { Link, useParams, useNavigate } from "react-router-dom";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faHouse } from "@fortawesome/free-solid-svg-icons";
-import Checkout from "../../Components/Checkout/Checkout";
-import { useSelector } from "react-redux";
-=======
 import React from 'react'
 import Header from '../../Components/Partials/Header/Header'
 import Footer from '../../Components/Partials/Footer/Footer'
@@ -16,18 +6,13 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faHouse } from '@fortawesome/free-solid-svg-icons'
 import Checkout from '../../Components/Checkout/Checkout'
 import ScrollToTop from '../ScrollToTop'
->>>>>>> 5ce94db8ed61ba961b9c069bd029c31007287af2
 
 export default function Checkout_page() {
   
   return (
     <>
-<<<<<<< HEAD
-      <Header />
-=======
     <ScrollToTop/>
         <Header/>
->>>>>>> 5ce94db8ed61ba961b9c069bd029c31007287af2
 
       <div className="breadcrum_box mt-2">
         <nav aria-label="breadcrumb">
